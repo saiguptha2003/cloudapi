990249b58cb25b7eabb2cc71f0ecbc0e0d6befb6
V D Panduranga Sai Guptha <96918090+saiguptha2003@users.noreply.github.com>
2024-03-13 11:42:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ app = Flask(__name__)
 def home(): 
     if request.method == 'GET': 
         data = "flaskserver"
-        return jsonify({'site name': data, 'message':'welcome to flaskServer' }) 
+        team_members = [
+            {"id": 10, "roll_number": "AP21110010053", "name": "KANDULA AYYAPPA", "grade": "A", "group": "Group-3"},
+            {"id": 11, "roll_number": "AP21110010064", "name": "JENIL PADSHALA", "grade": "A"},
+            {"id": 12, "roll_number": "AP21110010079", "name": "CHAGANTIPATI AETESH", "grade": "B"},
+            {"id": 13, "roll_number": "AP21110010081", "name": "BOLLINENI ROHITH", "grade": "B"},
+            {"id": 14, "roll_number": "AP21110010091", "name": "V D PANDURANGA SAI GUPTHA", "grade": "B"}
+        ]
+        return jsonify({'site_name': data, 'message': 'welcome to flaskServer', 'team_members': team_members, 'topic_name': 'amazon ec2'}) 
   
 # A simple function to calculate the square of a number 
 # the number to be squared is sent in the URL when we use GET 
