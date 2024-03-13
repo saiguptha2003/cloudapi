54a0a2c3cd25ef1b250f88da07be34685c9d816c
V D Panduranga Sai Guptha <96918090+saiguptha2003@users.noreply.github.com>
2024-03-13 10:40:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,21 @@ app = Flask(__name__)
 # on the terminal type: curl http://127.0.0.1:5000/ 
 # returns hello world when we use GET. 
 # returns the data that we send when we use POST. 
-@app.route('/', methods = ['GET', 'POST']) 
+@app.route('/', methods=['GET', 'POST']) 
 def home(): 
-    if(request.method == 'GET'): 
-  
+    if request.method == 'GET': 
         data = "hello world"
         return jsonify({'data': data}) 
   
-  
 # A simple function to calculate the square of a number 
 # the number to be squared is sent in the URL when we use GET 
-# on the terminal type: curl http://127.0.0.1:5000 / home / 10 
+# on the terminal type: curl http://127.0.0.1:5000/home/10 
 # this returns 100 (square of 10) 
-@app.route('/home/<int:num>', methods = ['GET']) 
+@app.route('/home/<int:num>', methods=['GET']) 
 def disp(num): 
-  
     return jsonify({'data': num**2}) 
-  
   
 # driver function 
 if __name__ == '__main__': 
-  
-    app.run(debug = True) 
+    # Run the app, listening on all available network interfaces
+    app.run(host='0.0.0.0', port=5000, debug=True)
